5bb57f2bb7634d8cb91831e7cfe6fd1decd7bade
sUeharaE4 <funa1hasi2hasi@gmail.com>
2020-01-31 09:24:33 +0900

    Change : reduce use global var and add comment

--- tests/test_json_key_list.py
+++ tests/test_json_key_list.py
@@ -19,29 +19,71 @@ INPUT_DIR = 'input'
 SEP = ': '
 
 
-def read_expect(expect_path):
+def read_expect(expect_path, sep):
+    """
+    期待値のtxtから改行を除いてdictを生成する.
+
+    Parameters
+    ----------
+    expect_path : str
+        期待値のPath.
+    sep : str
+        txtの1行をkey-valueに分割する区切り文字.
+
+    Returns
+    -------
+    expect_map : dict
+        txtを1行ずつkey-value形式で取得したdict.
+    """
     with open(expect_path, 'r') as f:
         # lines = f.readlines()
         lines = f.read().splitlines()
         expect_map = dict()
         for item in lines:
             tmp_line = str(item)
-            sep_index = tmp_line.find(SEP)
-            expect_map[tmp_line[:sep_index]] = tmp_line[sep_index + len(SEP):]
+            sep_index = tmp_line.find(sep)
+            expect_map[tmp_line[:sep_index]] = tmp_line[sep_index + len(sep):]
     return expect_map
 
 
 def read_input(input_path):
+    """
+    テスト対象のJSONを読み込みdictを生成する.
+
+    Parameters
+    ----------
+    input_path : str
+        テスト対象のJSONファイルPath.
+
+    Returns
+    -------
+    json_dict : dict
+        JSONを読み込み生成したdict.
+    """
     with open(input_path, 'r') as f:
         json_dict = json.load(f)
     return json_dict
 
 
 def __compare_util(input_json, expect, drop, base_name=None):
+    """
+    テスト対象と期待値のファイルを読み込み、変換結果と比較するUtility.
+
+    Parameters
+    ----------
+    input_json : str
+        テスト対象のJSONファイル名.
+    expect : str
+        期待値のファイル名.
+    drop : bool
+        json2line実行時にvalueが配列やObjectの場合値を捨てるか.
+    base_name : str
+        JsonPathの一番はじめの値(Noneの場合はデフォルト。削除はしない).
+    """
     input_json_path = os.path.join(INPUT_DIR, input_json)
     expect_path = os.path.join(EXPECT_DIR, expect)
     test_json = read_input(input_json_path)
-    expect_map = read_expect(expect_path)
+    expect_map = read_expect(expect_path, SEP)
 
     line_map = dict()
     if base_name is None:
@@ -51,8 +93,6 @@ def __compare_util(input_json, expect, drop, base_name=None):
 
     for key in line_map.keys():
         assert str(line_map[key]) == expect_map[key].replace('\"', '')
-
-
 
 
 @pytest.mark.parametrize('input_json, drop, expect', [
@@ -66,4 +106,3 @@ def test_basic_usage(input_json, drop, expect):
 
 def test_change_name():
     __compare_util('sample_min.json', 'sample_min_change_name.txt', False, 'OBJ')
-
